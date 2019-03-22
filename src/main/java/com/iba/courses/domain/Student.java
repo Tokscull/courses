a8baceb6b3e8f42e0f5d1be7772f29b4df3aded8
Tokscull <egor.kirilenko.99@gmail.com>
2019-03-22 11:58:09 +0300

    init

---
+++ src/main/java/com/iba/courses/domain/Student.java
@@ -0,0 +1,58 @@
+package com.iba.courses.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Student")
+public class Student {
+
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+
+    @Getter
+    @Setter
+    @Column(name = "name")
+    @NonNull
+    private String name;
+
+
+    @Getter
+    @Setter
+    @Column(name = "login")
+    @NonNull
+    private String login;
+
+
+    @Getter
+    @Setter
+    @Column(name = "password")
+    @NonNull
+    private String password;
+
+
+    @Getter
+    @Setter
+    @Column(name = "university")
+    @NonNull
+    private String university;
+
+
+    @Getter
+    @Setter
+    @Column(name = "course")
+    @NonNull
+    private Integer course;
+
+
+
+}
