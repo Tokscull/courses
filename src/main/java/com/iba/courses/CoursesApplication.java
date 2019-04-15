ae3426d80ed0cbe4bb8c70198531960b36cdbef4
Tokscull <egor.kirilenko.99@gmail.com>
2019-04-15 09:51:49 +0300

    refact

--- src/main/java/com/iba/courses/CoursesApplication.java
+++ src/main/java/com/iba/courses/CoursesApplication.java
@@ -3,14 +3,20 @@ package com.iba.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class CoursesApplication {
+public class CoursesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoursesApplication.class, args);
 
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return super.configure(builder);
+    }
 }
--- src/main/java/com/iba/courses/service/Db2Connect/Db2TerminalService.java
+++ src/main/java/com/iba/courses/service/Db2Connect/Db2TerminalService.java
@@ -6,10 +6,13 @@ import java.sql.SQLException;
 
 public class Db2TerminalService {
     public String Db2Terminal(Command command) {
+        System.out.println("Command message " + command.getMessage());
+        System.out.println("Command login " +command.getLogin());
+        System.out.println("Command password " +command.getPassword());
         Db2ConnectionService db2ConnectionService2 = new Db2ConnectionService();
         try {
             db2ConnectionService2.init(new Db2Parameters(),command.getLogin(),command.getPassword());
-           return db2ConnectionService2.execute(command.getMessage());  // -dis ddf
+            return db2ConnectionService2.execute(command.getMessage());  // -dis ddf
         } catch (SQLException e) {
             e.printStackTrace();
         }
